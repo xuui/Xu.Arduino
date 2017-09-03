e73b001d0804baf69081b670c698cf6de66d8e83
Xu.hel <xu.hel@outlook.com>
2017-09-03 21:27:27 +0800

    人体红外感应 代码简化
    Signed-off-by: Xu.hel <xu.hel@outlook.com>

---
+++ Sensor/Sensor.ino
@@ -0,0 +1,27 @@
+int Sensor_pin = 13;
+int Led_pin = 3;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(Sensor_pin,INPUT);
+  pinMode(Led_pin,OUTPUT);
+}
+void loop() {
+  int SensorState=digitalRead(Sensor_pin);
+  Serial.println(SensorState);
+
+  if(SensorState == 1){ 
+    Alarm();
+  }else{
+    return;
+  }
+  delay(100);
+}
+
+void Alarm(){
+  for(int i=0;i<100;i++){
+    digitalWrite(Led_pin,HIGH); //led 点亮
+    digitalWrite(Led_pin,LOW); //led 熄灭
+  }
+}
+
